dc38c0eb257cb078bccb655f5a6406e90bea805e
shaunpaw <164877944+shaunpaw@users.noreply.github.com>
2025-05-13 11:14:28 -0400

    Update DC_MOTOR.ino

--- DC_MOTOR/DC_MOTOR.ino
+++ DC_MOTOR/DC_MOTOR.ino
@@ -1,21 +1,23 @@
 #define ENA D5    // Motor speed control (PWM)
-#define IN1 D4   // Motor direction 1
-#define IN2 D3 
-#define IN3 D7 
-  // Motor direction 2
+#define IN1 D2   // Motor direction 1
+#define IN2 D1   // Motor direction 2
+
+#define IN3 D7   // variable for the TXS0108E 
+  
 
 void setup() {
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
-  digitalWrite(IN3,HIGH);
+  
+  digitalWrite(IN3,HIGH);  // sets the TXS0108E to HIGH
 }
 
 void loop() {
   // Move motor forward at half speed
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  analogWrite(ENA, 255); // PWM speed (0-255)
+  analogWrite(ENA, 128); // PWM speed (0-255)
 
   delay(2000);
 
